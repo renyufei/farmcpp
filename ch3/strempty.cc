90fc1801d8c79a95dd42dda76b3da2ad1c17b0ff
Yufei Ren <yufei.ren@stonybrook.edu>
2014-02-18 17:22:43 -0500

    add strempty

---
+++ ch3/strempty.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	string s;
+
+	if (s.empty())
+		cout << "It's an empty string" << endl;
+}
+
